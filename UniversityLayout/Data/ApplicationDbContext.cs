d408a24ec83e2a294fa335aa22385e205a0eb854
Jessica <suarez.jessica1992@hotmail.com>
2024-05-08 12:59:27 +0300

    db push / to fix migration

---
+++ UniversityLayout/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityLayout.Model;
+using UniversityLayout.Model.DataAnnotations;
+
+namespace UniversityLayout.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<FormA> FormA { get; set; }
+    }
+}
---
+++ UniversityLayout/Pages/ThankYou/ThankYouPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniversityLayout.Pages.ThankYou
+{
+    public class ThankYouPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
