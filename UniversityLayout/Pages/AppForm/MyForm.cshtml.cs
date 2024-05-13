b62295b9861812b49c2617899c83f85910d9b19b
Jessica <suarez.jessica1992@hotmail.com>
2024-05-13 20:13:52 +0300

    database added

--- UniversityLayout/Pages/AppForm/MyForm.cshtml.cs
+++ UniversityLayout/Pages/AppForm/MyForm.cshtml.cs
@@ -6,30 +6,29 @@ using UniversityLayout.Model.DataAnnotations;
 
 namespace UniversityLayout.Pages.AppForm
 {
-    public class FormModel : PageModel
+    public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
 
-        public FormModel(ApplicationDbContext db)
+        public CreateModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
         [BindProperty]
-        public FormA FormA { get; set; }
-
+        public FormA forma { get; set; }
         public void OnGet() { }
 
         public IActionResult OnPost()
         {
+            forma.CreatedAt = forma.UpdatedAt = DateTime.Now;
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-            _db.FormA.Add(FormA);
+            _db.FormA.Add(forma);
             _db.SaveChanges();
-
+            TempData["success"] = "New Form has been created.";
             return RedirectToPage("/ThankYou/ThankYouPage");
         }
     }
